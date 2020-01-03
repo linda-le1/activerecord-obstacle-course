cd05d5ef1fb8479f8ec97722fed371165173df9d
Linda Le <yueling1222@gmail.com>
2020-01-02 20:39:15 -0700

    Fix typo

--- spec/models/aroc_week_1_spec.rb
+++ spec/models/aroc_week_1_spec.rb
@@ -36,7 +36,7 @@ describe 'ActiveRecord Obstacle Course, Week 1' do
     # Solution goes here
     # Your solution should not contain the ID of the order anywhere
     order_id = Order.order(:amount).limit(1).pluck(:id)
-    order_id = Order.order(:amount).limit(1).first.id
+    order_id = Order.order(:amount).first.id
 
     # ------------------------------------------------------------
 
